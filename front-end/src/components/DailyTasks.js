7b65029fac6f5fdb92c748e84c0955067915340c
devindavis5 <devinrdavis555@gmail.com>
2020-11-29 13:39:39 -0600

    Before Adjusting Completion Column

--- front-end/src/components/DailyTasks.js
+++ front-end/src/components/DailyTasks.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { ListGroupItem, Form} from 'react-bootstrap';
-import Archive from '../assets/archive3.png'
+import Archive from '../assets/archive1.png'
+import Download from '../assets/download.png'
 import X from '../assets/x.png'
 import X2 from '../assets/x2.png'
 import Check from '../assets/check.png'
@@ -9,6 +10,8 @@ const DailyTasks = ({task, updateDailyTask}) => {
     const [formShow, setFormShow] = useState(false)
     const [description, setDescription] = useState(task.description)
     const [status, setStatus] = useState(task.status)
+    const [archived, setArchived] = useState(task.status === 'incomplete' ? false : true)
+
     const taskId = task.id
 
     const deleteProjectTask = () => {
@@ -37,27 +40,61 @@ const DailyTasks = ({task, updateDailyTask}) => {
         setStatus(task.status)
         setFormShow(!formShow)
     }
+
+    const findSource = () => {
+        let source
+        if (!archived) {
+            source = Archive
+        } else {
+            source = Download
+        }
+        return source
+    }
+
+    const toggleDailyTaskArchive = (e) => {
+        // e.preventDefault()
+        // setArchived(!)
+        // const taskData = {
+        //     description: description,
+        //     status: status,
+        //     id: taskId
+        // }
+        // updateDailyTask(taskData)
+        // formReset()  
+    }
     
     return (
         
         <>
         <tr >
         {!formShow ? 
+
             <td className="align-middle" id="daily-task-rows" onClick={() => resetEditForm()}>{task.description}</td>
         :
-            <td style={{width: "85%"}} class="align-middle">
+            <td style={{width: "90%"}} class="align-middle">
                 <Form.Control as="textarea" rows={1} value={description} onChange={e => setDescription(e.target.value)}/> 
             </td>
         }
         {!formShow ?
-            <td className="align-middle" id="daily-task-rows" size="sm" style={{ textAlign:"right" }}><img width="25" height="25" alt="archive" src={Archive}/></td>
+            
+            <td className="align-middle" id="daily-task-rows" size="sm" style={{ textAlign:"right" }}><img onClick={() => toggleDailyTaskArchive()} width="25" height="25" alt="archive" src={findSource()}/></td>
+            
         :
             <td className="align-middle"size="sm" style={{ textAlign:"right" }}>
                 <img width="13" onClick={e => taskSubmit(e)} height="18" alt="archive" src={Check}/>
                 <br/>
+                
                 <img width="15" onClick={() => formReset()} height="20" alt="archive" src={X2}/>
             </td>
         }
+
+        {archived ? 
+            
+            <td className="align-middle" id="archive-x" size="sm" style={{ textAlign:"right" }}><img width="15" height="20" alt="archive" src={X}/></td>
+        :
+            null
+        }
+        
         </tr>
         </>
     )   
--- front-end/src/pages/LandingPage.js
+++ front-end/src/pages/LandingPage.js
@@ -22,7 +22,8 @@ class LandingPage extends Component {
         newProjectName: '',
         userId: '',
         newDailyShow: false,
-        newTaskDescription: ''   
+        newTaskDescription: '',
+        dailyTaskArchiveShow: false   
     }
 
     componentDidMount() {
@@ -261,8 +262,13 @@ class LandingPage extends Component {
         this.setState({newTaskDescription: ''})
     }
 
+    toggleDailyTaskArchive = () => {
+        this.setState({dailyTaskArchiveShow: !this.state.dailyTaskArchiveShow})
+    }
+
     render() {
         const incompleteDailies = this.state.dailyTasks.filter(dt => dt.status === "incomplete")
+        const completeDailies = this.state.dailyTasks.filter(dt => dt.status === "complete")
         return (
             <div className='projects-page'>
                 <NavBar /> 
@@ -300,7 +306,7 @@ class LandingPage extends Component {
                     </CardDeck>
                 </div>
                 <div className='daily-tasks-div'>
-                    <Card id="daily-tasks-card" text="primary" >
+                    <Card id="daily-tasks-card" >
                         {/* <Card.Header id="daily-task-header" > */}
                         <Card.Title id="daily-task-title" className='text-center'>Today's Tasks
                         <img width="25" height="25" id="create-daily" alt="back" onClick={this.createDailyTaskFormToggle} src={New}/>
@@ -326,17 +332,43 @@ class LandingPage extends Component {
                                 }
                         {incompleteDailies.map(t => {
                             return (
-                            <DailyTasks task={t} key={t.id} updateDailyTask={this.updateDailyTask}/>) 
+                            <DailyTasks task={t} key={t.id} updateDailyTask={this.updateDailyTask}/>
+                            ) 
                         })}
                             </tbody>
                         </Table>
                         {/* </Card.Body> */}
                     </Card>
                     <div className="daily-task-archive-div">
-                        <img alt="archive" id="daily-task-archive" src={Archive1}/>  
+                        <img onClick={this.toggleDailyTaskArchive} alt="archive" id="daily-task-archive" src={Archive1}/>
+
+
+                <Modal
+                    show={this.state.dailyTaskArchiveShow}
+                    onHide={this.toggleDailyTaskArchive}
+                    dialogClassName="modal-90w"
+                    size="xl"
+                    >
+                    <Modal.Header closeButton>
+                        <h1>Archived Daily Tasks</h1>
+                    </Modal.Header>
+                    <Modal.Body>
+                    <Table responsive className="table-hover">
+                        <tbody>
+                        {completeDailies.map(t => {
+                            return (
+                                <DailyTasks task={t} key={t.id} updateDailyTask={this.updateDailyTask}/>
+                            )  
+                        })}
+                        </tbody>
+                    </Table>
+                    </Modal.Body>
+                </Modal>       
+
+
                     {/* </div>  
                     <div className="project-archive-div"> */}
-                        <img alt="archive" id="project-archive" src={Archive2}/>  
+                        <img  alt="archive" id="project-archive" src={Archive2}/>  
                     {/* </div>  
                     <div className="daily-task-archive-div"> */}
                         <img alt="archive" id="project-task-archive" src={Archive3}/>  
